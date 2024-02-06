646733d97c463de65b34f29a9a623a29bd580e50
ToastyThe1 <158803238+ToastyThe1@users.noreply.github.com>
2024-02-05 23:01:21 -0700

    Made a quick story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+print('On a rainy day, a young man was leaving work to head home')
+print('When a truck lost control and hit this man')
+print('The man knew he was about to die, one thing he wish was that he lived a more exciting life')
+print(' As he's took his final breath and closed his eys, he saw a light')
+print('As he got to the light, next moment he finds his self in the arms of a women')
+print(' He was reborn in another world')
+# Author: Student Toate Sika
+# My first Python lab in my CIS129 course
+# Prints a simple short story
